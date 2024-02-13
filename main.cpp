67a5a6beb44b716a4b0149aadf8e1d91e0396ef4
Xandru Mifsud <xandru.mifsud@gmail.com>
2024-02-13 17:48:06 +0100

    Added exposed program options

--- main.cpp
+++ main.cpp
@@ -6,6 +6,9 @@
 
 using namespace std;
 
+#include <boost/program_options.hpp>
+using namespace boost::program_options;
+
 #include "BlockingPattern/blocking_pattern.h"
 #include "SATInstance.h"
 
@@ -13,20 +16,79 @@ typedef uint64_t UINT_T;
 typedef SATInstance<UINT_T>::ClauseArray ClauseArray;
 typedef uint8_t byte;
 
-void bits_to_bmp(VariablesArray<UINT_T>* data, UINT_T n, char* out);
 Clause<UINT_T>* getEnumeratedClause(UINT_T, unsigned short int);
 string get_mask(int h, int b);
 
 auto blocking_patterns = new vector<BlockingPattern<UINT_T>*>();
 
-int main() {
-    unsigned short int n_threads = 8;
-
-    UINT_T n = 50;
+int main(int argc, char *argv[]){
+    UINT_T n = 600;
     UINT_T n_clauses = 0;
-    UINT_T batch_size = 50000000;
+    UINT_T batch_size = 100000;
 
-    auto* bp1 = new BlockingPattern<UINT_T>(get_mask(9, 8), n);
+    unsigned short int n_threads = 0;
+
+    int h = 10;
+    int b = 9;
+
+    bool output = false;
+
+    try {
+        options_description desc{"Options"};
+        desc.add_options()
+                ("help", "Help")
+                ("parallel,p", value<unsigned short int>()->default_value(0), "Use parallel solver")
+                ("batch_size,c", value<UINT_T>()->required(), "Clause batch-size per thread")
+                ("dimension,n", value<UINT_T>()->required(), "Grid dimension")
+                ("mask_size,h", value<int>()->required(), "Mask dimension h x h")
+                ("diagonal,b", value<int>()->required(), "Number of black cells b <= h on the diagonal")
+                ("output,o", "Output instance as DIMACS CNF?");
+
+        variables_map vm;
+        store(parse_command_line(argc, argv, desc), vm);
+
+        if(vm.count("help")) {
+            std::cout << desc << '\n';
+            exit(0);
+        } else {
+            notify(vm);
+
+            if(vm.count("output")){
+                output = true;
+            }
+
+            if(vm.count("parallel")){
+                if (vm["parallel"].as<unsigned short int>() < 0 || vm["parallel"].as<unsigned short int>() > omp_get_num_procs()) {
+                    n_threads = omp_get_num_procs();
+                } else if (vm["parallel"].as<unsigned short int>() > 0) {
+                    n_threads = vm["parallel"].as<unsigned short int>();
+                } else {
+                    n_threads = 1;
+                }
+            }
+
+            if (vm.count("batch_size")) {
+                batch_size = vm["batch_size"].as<UINT_T>();
+            }
+
+            if (vm.count("dimension")) {
+                n = vm["dimension"].as<UINT_T>();
+            }
+
+            if (vm.count("mask_size")) {
+                h = vm["mask_size"].as<int>();
+            }
+
+            if (vm.count("diagonal")) {
+                b = vm["diagonal"].as<int>();
+            }
+        }
+    } catch (const error &e) {
+        cerr << e.what() << endl;
+        exit(1);
+    }
+
+    auto* bp1 = new BlockingPattern<UINT_T>(get_mask(h, b), n);
 
     blocking_patterns->push_back(bp1); n_clauses += bp1->n_clauses;
 
@@ -48,13 +110,12 @@ int main() {
     cout << "\n\nAvg. UNSAT MIS Size = " + to_string(statistics->avg_mis_size)
             + "\n-------------------------------------\n\n";
 
-    if (n < 256) {
-        bits_to_bmp(var_arr, n, (char*) "./out.bmp");
+    if (output) {
+        string f_name = "n" + to_string(n) + "_h" + to_string(h) + "_b" + to_string(b) + ".cnf";
+        auto out_f = new ofstream(f_name);
+        satInstance->writeDIMACS(getEnumeratedClause, out_f);
+        out_f->close();
     }
-
-    auto out_f = new ofstream("./sat.cnf");
-    satInstance->writeDIMACS(getEnumeratedClause, out_f);
-    out_f->close();
 
     return 0;
 }
@@ -94,65 +155,4 @@ string get_mask(int h, int b) {
     }
 
     return mask;
-}
-
-byte* get_bmp_file_header() {
-    static byte header[14];
-
-    header[0] = (byte) 'B';
-    header[1] = (byte) 'M';
-    header[10] = (byte) 54;
-
-    return header;
-}
-
-byte* get_bmp_info_header(UINT_T n) {
-    static byte header[40];
-
-    header[0] = (byte) 40;
-    header[4] = (byte) n;
-    header[5] = (byte) n >> 8;
-    header[6] = (byte) n >> 16;
-    header[7] = (byte) n >> 24;
-    header[8] = (byte) n;
-    header[9] = (byte) n >> 8;
-    header[10] = (byte) n >> 16;
-    header[11] = (byte) n >> 24;
-    header[12] = (byte) 1;
-    header[14] = (byte) 1;
-    header[32] = (byte) 2;
-    header[36] = (byte) 2;
-
-    return header;
-}
-
-void bits_to_bmp(VariablesArray<UINT_T>* data, UINT_T n, char* out) {
-    FILE* fout = fopen(out, "wb");
-
-    fwrite(get_bmp_file_header(), sizeof(byte), 14, fout);
-    fwrite(get_bmp_info_header(n), sizeof(byte), 40, fout);
-
-    byte black_colour[4] = {0,0,0,0};
-    fwrite(black_colour, 1, 4, fout);
-
-    byte white_colour[4] = {0xff,0xff,0xff,0xff};
-    fwrite(white_colour, 1, 4, fout);
-
-    UINT_T offset, numBytes = ((n + 31) / 32) * 4;
-    byte bytes[numBytes];
-
-    for (UINT_T y = 0; y < n; y++) {
-        offset = (n - y - 1) * n;
-        memset(bytes, 0, (numBytes * sizeof(byte)));
-
-        for (UINT_T x = 0; x < n; ++x) {
-            if (!data->vars[offset++]) {
-                bytes[x / 8] |= 1 << (7 - x % 8);
-            }
-        }
-
-        fwrite(bytes, sizeof(byte), numBytes, fout);
-    };
-
-    fclose(fout);
 }
